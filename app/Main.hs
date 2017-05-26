c07e9167e4db0a8ffaee5542da22ba525576f835
Daisuke <Yamagutino@greenApple.local>
2017-05-26 16:44:19 +0900

    orderN is refined to StateMonad

--- app/Main.hs
+++ app/Main.hs
@@ -2,20 +2,27 @@ module Main where
 
 import Lib
 import HECC
+import Control.Monad
+import Control.Monad.State
 
 main :: IO ()
 main = someFunc
 
 basePoint :: (Int,Int)
-basePoint = head $ filter (\l -> (snd l^2) `eqMod5557` (fst l^3 + 17*fst l + 173)) [(x,y)|x<-[0..5556],y<-[0..5556]]
+basePoint = head $ filter infCondition [(x,y)|x<-[0..5556],y<-[0..5556]]
   where
     eqMod5557 alpha beta = alpha `mod` 5557 == beta `mod` 5557
+    infCondition l = (snd l^2) `eqMod5557` (fst l^3 + 17*fst l + 173)
 
-orderN :: HECC5557 -> (Int,Int) -> Int
-orderN Infinity _ = 0
-orderN rho (x,y) = head $ filter (\n -> n × rho == Infinity ) [x..y]
+orderN :: Int -> State HECC5557 Int
+orderN n = do
+  p <- get
+  case p of
+    Infinity -> return n
+    Point x y -> do
+                 put (p + g)
+                 orderN (succ n)
 
---point G
 g = Point 0 2032
 
 --n G = Infinity
